84c69180a199710ef9fab147ba341757741644b5
KlaudeLam <kaileylammyky@gmail.com>
2025-01-31 23:40:10 +0400

    Display full caption

--- js/IHDs-Blog.js
+++ js/IHDs-Blog.js
@@ -16,19 +16,27 @@ onValue(postRef, (snapshot) => {
   // Convert Time
   const d = new Date(data.time);
   const time = d.toDateString();
+  let caption = "";
+  const caption_block = data.content.blocks;
+  for (let i = 0; i < caption_block.length; i++) {
+    caption += caption_block[i].data.text + "<br>";
+    // console.log(caption_block[i].data.text, "\n");
+  }
+  // console.log(caption);
   // Display information
   document.querySelector("#info").innerHTML = `
     <img src="${data.thumbnail}" alt="" class="w-full rounded object-cover" />
     <h1 class="text-4xl font-bold leadi md:text-5xl">
       ${data.title}
     </h1>
-    <p class="text-sm dark:text-gray-400">
+    <p>${caption}</p>
+    <p class="text-sm" style="color:gray">
       by
       <a
         rel="noopener noreferrer"
         href="#"
         target="_blank"
-        class="underline dark:text-violet-400"
+        class="underline text-black"
       >
         <span itemprop="name">${data.name}</span></a
       > on
@@ -38,29 +46,29 @@ onValue(postRef, (snapshot) => {
   console.log(data);
 
   // Editor ---------------------------------------------------------------------
-  const editor = new EditorJS({
-    holder: "editor",
-    placeholder: "Let`s write an awesome story!",
-    readOnly: true,
-    tools: {
-      header: {
-        class: Header,
-      },
-      image: SimpleImage,
-      checklist: {
-        class: Checklist,
-        inlineToolbar: true,
-      },
-      list: {
-        class: List,
-        inlineToolbar: true,
-        config: {
-          defaultStyle: "unordered",
-        },
-      },
-      embed: Embed,
-      quote: Quote,
-    },
-    data: data.content,
-  });
+  //   const editor = new EditorJS({
+  //     holder: "editor",
+  //     placeholder: "Let`s write an awesome story!",
+  //     readOnly: true,
+  //     tools: {
+  //       header: {
+  //         class: Header,
+  //       },
+  //       image: SimpleImage,
+  //       checklist: {
+  //         class: Checklist,
+  //         inlineToolbar: true,
+  //       },
+  //       list: {
+  //         class: EditorjsList,
+  //         inlineToolbar: true,
+  //         config: {
+  //           defaultStyle: "unordered",
+  //         },
+  //       },
+  //       embed: Embed,
+  //       quote: Quote,
+  //     },
+  //     data: data.content,
+  //   });
 });
